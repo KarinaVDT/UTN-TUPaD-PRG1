23aac75c139f58e2cf14043831ae075633c52a64
Karina Apellido <karina.dangelo@utn.edu.ar>
2025-09-13 21:38:26 -0300

    Renombrar archivo Ud_5_listas a Ud_5_listas.py

--- TP5_Listas/Ud_5_listas.py
+++ TP5_Listas/Ud_5_listas.py
@@ -1,4 +1,4 @@
- 1) Crear una lista con las notas de 10 estudiantes.
+# 1) Crear una lista con las notas de 10 estudiantes.
 #• Mostrar la lista completa.
 #• Calcular y mostrar el promedio.
 #• Indicar la nota más alta y la más baja
